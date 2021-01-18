dd9bebf4c8f8b137271fa41193c7fda1b2f4cbdb
Rudy Saavedra <rudysp@hotmail.com>
2021-01-18 01:00:36 -0400

    segunda prueba

---
+++ src/app/componentes/componentes.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    IonicModule
+  ],
+  exports:[
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule, IonicModule
+  ]
+})
+export class ComponentesModule { }
